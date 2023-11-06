c02141992e029f71084b8294c5cc71096683b20d
Shubham Saha <imshubhamsaha@gmail.com>
2023-11-07 01:49:28 +0530

    adding different comment for differnt routes

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.write("<html>");
-  res.write("<head><title>My first backend server</title></head>");
-  res.write("<body><h1>Shubham Saha</h1></body>");
-  res.write("</html>");
-  res.end();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>My first backend server</title></head>");
+    res.write("<body><h1>Welcome to home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>My first backend server</title></head>");
+    res.write("<body><h1>Welcome to About us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>My first backend server</title></head>");
+    res.write("<body><h1>Welcome to my first Node.js Server</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
 });
 
 server.listen(4000);
