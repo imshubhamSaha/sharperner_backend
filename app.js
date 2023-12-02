60448257f53c0f53b23e0f2e143f361352d58abd
Shubham Saha <imshubhamsaha@gmail.com>
2023-12-03 01:39:09 +0530

    task-2 added MW for post req

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the Middle ware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<html><body><form action="/product" method="POST"><input type="text" name = "title" placeholder="product"><input type="number" name="size" placeholder="size"><button type="submit">Add Product</button></form></body></html>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("In the another Middle ware");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
   //   res.send({ key1: "value" });
   res.send("<h1> hello to node js </h1>");
 });
